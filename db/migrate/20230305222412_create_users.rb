97ccf59317cd664e13fcc2635a96e51bafd3ee31
RichardOkongo <rirachard19@gmail.com>
2023-03-06 03:36:16 +0300

    created new methods for the user and memes

--- db/migrate/20230305222412_create_users.rb
+++ db/migrate/20230305222412_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.
-
+      t.string :name 
+      t.integer :user_id
+    end
   end
 end
--- db/migrate/20230305222437_create_memes.rb
+++ db/migrate/20230305222437_create_memes.rb
@@ -1,4 +1,7 @@
 class CreateMemes < ActiveRecord::Migration[6.1]
   def change
+    create_table :memes do |t|
+      t.string :name
+    end
   end
 end
