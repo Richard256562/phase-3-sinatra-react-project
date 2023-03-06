659750ed9ec11467e38a1747b3d7459fc4cdc099
RichardOkongo <rirachard19@gmail.com>
2023-03-06 06:25:31 +0300

    created seeds

--- db/migrate/20230306002933_add_user_id.rb.rb
+++
@@ -1,5 +0,0 @@
-class AddUserId.rb < ActiveRecord::Migration[6.1]
-  def change
-        add_reference :memes, :user, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_03_06_032119) do
 
+  create_table "memes", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_memes_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+  end
+
+  add_foreign_key "memes", "users"
 end
